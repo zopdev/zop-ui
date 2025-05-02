48957c0c16188d5f8a8e3cae4ff6cfa42ccb4253
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2025-05-02 10:46:44 +0530

    fixed the error component word break issue

--- components/ErrorComponent/index.js
+++ components/ErrorComponent/index.js
@@ -7,9 +7,9 @@ export default function ErrorComponent({ errorText, className }) {
         <div className="flex-shrink-0">
           <ExclamationCircleIcon className="h-6 w-6 text-red-500" aria-hidden="true" />
         </div>
-        <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-600 text-opacity-70 break-all">
-            {errorText}
+        <div className="ml-3 min-w-0">
+          <h3 className="text-sm font-medium text-red-600 text-opacity-70 break-words">
+            {errorText || 'Something went wrong'}
           </h3>
         </div>
       </div>
